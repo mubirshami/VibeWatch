aa9a6f62fa32b2225c6f93acd8a81c39641e7837
mubirshami <mubirshami2000@gmail.com>
2024-10-17 01:55:57 +0500

    Added morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
+app.use(helmet());
+app.use(morgan('common'));
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
