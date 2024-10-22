50e363c9ae2c3a9fa0f8f9f4c1082287b707ac11
mubirshami <mubirshami2000@gmail.com>
2024-10-22 18:41:25 +0500

    JWT Added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,13 +45,35 @@ const userSchema = mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if(!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password,8);
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 8);
   next();
-})
+});
 
-userSchema.method.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
-}
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: process.env.TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
